57d9498fe24fc453eca4f302afc5ffd1e83bb8b4
Camila Martins <37345577+camila-martins@users.noreply.github.com>
2018-04-12 00:17:43 -0300

    Update codigo.c

--- codigo.c
+++ codigo.c
@@ -53,6 +53,50 @@ void imprime(double **M, int dim)
         }
 }
 
+//Função para trocar as linhas da matriz
+void trocalinha(double *M1, double *M2, int dim)
+{	double *m; // matriz auxiliar
+	int i;
+	
+	m= (double*)malloc((dim+1)*sizeof(double));
+	
+	for(i=0;i<=dim;i++)
+	{	m[i] = M1[i];
+		M1[i] = M2[i];
+		M2[i] = m[i];
+	}
+}
+
+//Função que tiangulariza a matriz
+double **triangsup(double **M, int dim)
+{
+ 	int i, j, k, l, vaux;
+ 	double N;
+ 	vaux=-1;
+ 	for(k=0;k<dim;k++) 
+ 	{	
+ 		for(l=k;l<dim;l++)
+			if(M[k][k]<fabs(M[l][k]))
+				vaux = l;
+ 			if(vaux!=-1)
+			{	
+				trocalinha(M[k],M[vaux],dim);
+			}
+ 
+ 		for(i=k+1;i<dim;i++)
+ 		{	
+			 N=(M[i][k]/M[k][k]);
+ 			 for(j=k;j<=dim;j++)
+ 			 {
+ 			 	M[i][j]=M[i][j]-(N*M[k][j]);
+ 			 }
+ 		}
+ 		vaux=-1;
+ 	}
+	
+	return (M);
+}
+
 //Função substituiçao reversa para encontrar a solução geral
 void subsreversa( double **M, double *raizes, int dim)
 {
@@ -79,6 +123,9 @@ int main (int argc , char **argv )
   printf("\nMatriz lida no arquivo Matriz.dat:\n");
   M=ler(argv[1] ,&dim);
   imprime(M,dim);
+   printf("\nMatriz triangularizada:\n");
+  M=triangsup(M, dim);
+  imprime(M, dim);
   //Solução atraves da substituição reversa:
   printf("\nSolução:\n");
   raizes = malloc(dim *sizeof(double));
@@ -87,7 +134,7 @@ int main (int argc , char **argv )
   {
   	printf ("x%1d = %5.2lf\n",i , raizes[i]) ;
   }
-  return 0;
+  return (0);
   fclose(fp);
 }
  
